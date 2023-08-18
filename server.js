87c152733bdd413d129f897b586c5d063e8f2219
Kolten ✔️ <kolten.pilar@gmail.com>
2023-08-18 14:22:12 -0400

    connected my database, can see json in the browser

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const pokemon = require('./models/pokemon.js'); //import pokemon data
+
 //setting up view engine
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jsx');
@@ -14,6 +16,10 @@ app.get('/', (req, res) => {
   res.send(`<h1>Welcome to the Pokemon App!</h1>`)
 })
 
+//pokemon get route
+app.get('/pokemon', (req, res) => {
+  res.send(pokemon);
+})
 
 app.listen(port, () => {
   console.log(`listening at port ${port}`);
