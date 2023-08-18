fd1cdfd478bd79cec1469ebbb7b4938ce42b269b
Kolten ✔️ <kolten.pilar@gmail.com>
2023-08-18 14:44:53 -0400

    i can view a list of all my pokemon capitalized in the browser

--- views/Index.jsx
+++ views/Index.jsx
@@ -1,19 +1,22 @@
 import React from "react";
 
 const myStyle = {
-  color: '#fff',
-  backgroundColor: '#000'
-}
+  color: "#fff",
+  backgroundColor: "#000",
+};
 
 function Index({ pokemon }) {
   return (
     <div style={myStyle}>
       <h1>See All The Pokemon</h1>
       {pokemon.map((pokemon, i) => {
+        const capitalName = (pokemon.name).charAt(0).toUpperCase() + (pokemon.name).slice(1) //slice starts at the second character then returns the rest of the letters
         return (
-          <li key={i}>
-            <a href={`/pokemon/${i}`}>{pokemon.name}</a> {pokemon.img} <br />
-          </li>
+          <ul key={i}>
+            <li >
+              <a href={`/pokemon/${i}`}>{capitalName}</a> {pokemon.img} <br />
+            </li>
+          </ul>
         );
       })}
     </div>
